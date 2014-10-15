c165c7d85615086c47acadb56acf36d402496570
Gordie Smith <agsmith4@gmail.com>
2014-10-15 13:47:30 -0400

    Finished most basic functionality possible

--- scripts.js
+++ scripts.js
@@ -1,20 +1,28 @@
-
 $(document).ready(function() {
 
-	createGrid(prompt("What size grid do you want?"));
+	createGrid(prompt("Enter a grid size 1-20"));
 
-});
-
+// Creating a grid
 
 function createGrid(x) {
-
-	var boxSize = (300 / x);
 	
 	for (var i = 0; i < (x * x); i++) {
 	
 	$('.wrap').append('<div class="box"></div>');
-	
 	}
+
+// Sizing the boxes within wrap div
+
+	var boxSize = (500 / x);
+
 	$('.box').width(boxSize);
 	$('.box').height(boxSize);
-}
+};
+
+// Change div color on Mouse Enter
+
+$('.box').mouseenter(function() {
+$(this).fadeTo("slow", .2);
+});
+
+});
